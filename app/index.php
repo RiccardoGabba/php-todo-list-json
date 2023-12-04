d0c1671b6ecdccd99cc5eccff47dd9bddf8ac92c
filippo <riccardo.gabba02@gmail.com>
2023-12-04 16:08:37 +0100

    fixed the css

--- app/index.php
+++ app/index.php
@@ -3,21 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Document</title>
 </head>
 <body>
     <div class="list">
-        <h1>To Do</h1>
+        <h1>To Do </h1>
 
-        <ul>
+        <ul class="items">
+            
             <li>
-                <span class="item">ciao</span>
+                <span class="item ">ciao</span>
                 <a href="#" class="done-button">Mark as done</a>
             </li>
+            
         </ul>
+        
 
         <form action="add.php" method="post" class="item-add">
-            <input type="text" class="input" name="name" placeholder="Type a new item here." autocomplete="off" required>
+            <input type="text" name="name" placeholder="Type  a new item here." class="input" autocomplete="off" required>
             <input type="submit" value="Add" class="submit">
         </form>
     </div>
